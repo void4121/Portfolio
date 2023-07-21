735e157f20968d61751b4d57798ef463e6a7c5c9
Rick <ramspam43@gmail.com>
2023-07-21 11:22:33 -0400

    made modifcation to home and about page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,33 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
+            Hi Everyone, I am <span className="purple">Ricardo Ramirez </span>
+            from <span className="purple"> New york, NY.</span>
+            <br /> I am a final year student pursuing Computer Science & Information Systems
+            at John Jay of Criminal Justice.
             <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
+            Additionally, I am currently employed as a Junior Network Technician.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from IT, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Video Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Hanging with friends
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> 3D printing
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "You have power over your mind — not outside events. 
+            Realize this and you will find strength."{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Marcus Aurelius</footer>
         </blockquote>
       </Card.Body>
     </Card>
