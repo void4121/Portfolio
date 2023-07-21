bbde5bd1b1d6aa1a87ebe4a878b589a61abd48c0
Rick <ramspam43@gmail.com>
2023-07-21 10:13:22 -0400

    update typewriter

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,8 +7,8 @@ function Type() {
       options={{
         strings: [
           "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
+          "IT pro",
+          "Service hoster",
           "Open Source Contributor",
         ],
         autoStart: true,
