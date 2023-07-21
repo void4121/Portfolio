effcc561bab1cc79604a0f713e6a4cba8357d7bf
Rick <ramspam43@gmail.com>
2023-07-21 08:46:18 -0400

    uncommented github icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-//import {
-//  AiFillGithub,
+import {
+  AiFillGithub
 //  AiOutlineTwitter,
 //  AiFillInstagram,
-//} from "react-icons/ai";
+} from "react-icons/ai";
 //import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
