e444beeb047c9548ee0fac720964603706a60964
Rick <ramspam43@gmail.com>
2023-08-01 13:15:44 -0400

    updated link

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,13 +21,8 @@ function Projects() {
           See something you would like setup or need assistance with? <br />
           Submit a ticket via 
         </p> 
-        <Button
-            href="https://leet-tech.atlassian.net/servicedesk/customer/portals"
-            target="_blank"
-            className="fork-btn-inner"
-        ></Button>
         <body>
-        <a href="https://leet-tech.atlassian.net/servicedesk/customer/portals" target="_blank" rel="noreferrer">Leet-tech Ticketing Portal</a>
+          <a href="https://leet-tech.atlassian.net/servicedesk/customer/portals" target="_blank" rel="noreferrer">Leet-tech Ticketing Portal</a>
         </body>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
