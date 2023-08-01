b3a3a447b5ae4d64d52dfc5ddddbc3a0ad783913
Rick <ramspam43@gmail.com>
2023-08-01 13:15:31 -0400

    updated link

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,8 +26,9 @@ function Projects() {
             target="_blank"
             className="fork-btn-inner"
         ></Button>
+        <body>
         <a href="https://leet-tech.atlassian.net/servicedesk/customer/portals" target="_blank" rel="noreferrer">Leet-tech Ticketing Portal</a>
-
+        </body>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
